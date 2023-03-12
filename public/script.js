84016d0f6d24ed12c22de91f35f276ae883ff4dd
sukhe <suhbat8575@gmail.com>
2023-03-12 17:04:08 +0800

    files

---
+++ public/script.js
@@ -0,0 +1,229 @@
+const batsuuri = document.getElementById("batsuuri");
+const modal = document.querySelector(".modal");
+
+batsuuri.addEventListener("click", ()=>{
+    modal.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e) => {
+    if(e.target == modal){
+        modal.style.visibility = "hidden";
+    }
+});
+
+
+const burentugs = document.getElementById("burentugs");
+const modal1 = document.querySelector(".modal1");
+
+burentugs.addEventListener("click", () =>{
+    modal1.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal1){
+        modal1.style.visibility = "hidden";
+    }
+});
+
+const sanjaadamba = document.getElementById("sanjaadamba");
+const modal2 = document.querySelector(".modal2");
+
+sanjaadamba.addEventListener("click", () =>{
+    modal2.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal2){
+        modal2.style.visibility = "hidden";
+    }
+});
+
+const hangai = document.getElementById("hangai");
+const modal3 = document.querySelector(".modal3");
+
+hangai.addEventListener("click", () =>{
+    modal3.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal3){
+        modal3.style.visibility = "hidden";
+    }
+});
+
+const orhonbayr = document.getElementById("orhonbayr");
+const modal4 = document.querySelector(".modal4");
+
+orhonbayr.addEventListener("click", () =>{
+    modal4.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal4){
+        modal4.style.visibility = "hidden";
+    }
+});
+
+const badarch = document.getElementById("badarch");
+const modal5 = document.querySelector(".modal5");
+
+badarch.addEventListener("click", () =>{
+    modal5.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal5){
+        modal5.style.visibility = "hidden";
+    }
+});
+
+
+const zorigtbaatar = document.getElementById("zorigtbaatar");
+const modal6 = document.querySelector(".modal6");
+
+zorigtbaatar.addEventListener("click", () =>{
+    modal6.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal6){
+        modal6.style.visibility = "hidden";
+    }
+});
+
+const zolboo = document.getElementById("zolboo");
+const modal7 = document.querySelector(".modal7");
+
+zolboo.addEventListener("click", () =>{
+    modal7.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal7){
+        modal7.style.visibility = "hidden";
+    }
+});
+
+const batmagnai = document.getElementById("batmagnai");
+const modal8 = document.querySelector(".modal8");
+
+batmagnai.addEventListener("click", () =>{
+    modal8.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal8){
+        modal8.style.visibility = "hidden";
+    }
+});
+
+const sosorbaram = document.getElementById("sosorbaram");
+const modal9 = document.querySelector(".modal9");
+
+sosorbaram.addEventListener("click", () =>{
+    modal9.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal9){
+        modal9.style.visibility = "hidden";
+    }
+});
+
+const munhjargal = document.getElementById("munhjargal");
+const modal10 = document.querySelector(".modal10");
+
+munhjargal.addEventListener("click", () =>{
+    modal10.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal10){
+        modal10.style.visibility = "hidden";
+    }
+});
+
+const naranbayr = document.getElementById("naranbayr");
+const modal11 = document.querySelector(".modal11");
+
+naranbayr.addEventListener("click", () =>{
+    modal11.style.visibility = "unset";
+});
+
+window.addEventListener("click", (e)=>{
+    if(e.target == modal11){
+        modal11.style.visibility = "hidden";
+    }
+});
+
+const batsuuri1 = document.getElementById("batsuuri1");
+
+batsuuri1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch/?ref=search&v=667910907931435&external_log_id=05cd23e2-0d68-4e39-ba1a-c8ad63455124&q=%D0%B1%D0%B0%D1%82%D1%81%D1%83%D1%83%D1%80%D1%8C%20%D0%B0%D0%B2%D0%B0%D1%80%D0%B3%D0%B0"
+};
+
+const burentugs1 = document.getElementById("burentugs1");
+
+burentugs1.onclick = () =>{
+    location.href = "https://www.facebook.com/mongoltv/videos/565013101742648"
+};
+
+const sanjaadamba1 = document.getElementById("sanjaadamba1");
+
+sanjaadamba1.onclick = () =>{
+    location.href = "https://www.facebook.com/100079996045232/videos/689545452702950"
+};
+
+const hangai1 = document.getElementById("hangai1");
+
+hangai1.onclick = () =>{
+    location.href = "https://www.facebook.com/mongoltv/videos/378451664413522"
+};
+
+const orhonbayr1 = document.getElementById("orhonbayr1");
+
+orhonbayr1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch/?ref=search&v=297496472196723&external_log_id=8167d963-50b7-4d0b-86f4-7770d975b921&q=%D0%BE%D1%80%D1%85%D0%BE%D0%BD%D0%B1%D0%B0%D1%8F%D1%80%D1%8B%D0%BD%20%D0%B1%D0%B0%D1%80%D0%B8%D0%BB%D0%B4%D0%B0%D0%B0%D0%BD"
+};
+
+const badarch1 = document.getElementById("badarch1");
+
+badarch1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch?ref=search&v=2788380001396278&external_log_id=27e6ae56-d1b4-43e6-a984-bb4aadf3da97&q=%D0%B1%D0%B0%D0%B4%D0%B0%D1%80%D1%87%20%D0%B3%D0%B0%D1%80%D1%8C%D0%B4%20%D0%B1%D0%B0%D1%80%D0%B8%D0%BB%D0%B4%D0%B0%D0%B0%D0%BD"
+};
+
+const zorigtbaatar1 = document.getElementById("zorigtbaatar1");
+
+zorigtbaatar1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch/?ref=search&v=3183590451742640&external_log_id=0651a9c5-9739-4b9c-9f35-86925c513e9c&q=%D0%B7%D0%BE%D1%80%D0%B8%D0%B3%D1%82%D0%B1%D0%B0%D0%B0%D1%82%D0%B0%D1%80%20%D1%85%D0%B0%D1%80%D1%86%D0%B0%D0%B3%D0%B0"
+};
+
+const zolboo1 = document.getElementById("zolboo1");
+
+zolboo1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch/?ref=search&v=1855942671132896&external_log_id=95f6b0a9-83f0-4eff-89b7-5ce3299ad9a3&q=%D0%B7%D0%BE%D0%BB%D0%B1%D0%BE%D0%BE%20%D1%85%D0%B0%D1%80%D1%86%D0%B0%D0%B3%D0%B0"
+};
+
+const batmagnai1 = document.getElementById("batmagnai1");
+
+batmagnai1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch/?ref=search&v=997870450908417&external_log_id=23834375-fe43-48aa-8cf8-4ea83542b947&q=%D0%B1%D0%B0%D1%82%D0%BC%D0%B0%D0%B3%D0%BD%D0%B0%D0%B9%20%D0%BD%D0%B0%D1%87%D0%B8%D0%BD"
+};
+
+const sosorbaram1 = document.getElementById("sosorbaram1");
+
+sosorbaram1.onclick = () =>{
+    location.href = "https://www.facebook.com/ddishofficial/videos/1019724879417729"
+};
+
+const munhjargal1 = document.getElementById("munhjargal1");
+
+munhjargal1.onclick = () =>{
+    location.href = "https://www.facebook.com/100029048381118/videos/1092717124827686/?idorvanity=852438185461063"
+};
+
+const naranbayr1 = document.getElementById("naranbayr1");
+
+naranbayr1.onclick = () =>{
+    location.href = "https://www.facebook.com/watch?ref=search&v=1078124956248472&external_log_id=fb86f45d-17f0-483b-9d9d-b3eeba6cd5aa&q=%D0%BD%D0%B0%D1%80%D0%B0%D0%BD%D0%B1%D0%B0%D1%8F%D1%80%20%D1%85%D0%B0%D1%80%D1%86%D0%B0%D0%B3%D0%B0"
+};
